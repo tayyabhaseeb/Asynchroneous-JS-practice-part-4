6b4db6ff2ce8f0b8916f427cc60b03884236281a
Tayyab Haseeb <73613576+tayyabhaseeb@users.noreply.github.com>
2023-08-17 23:51:00 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,53 +2,78 @@ let deckId
 const cardsContainer = document.getElementById("cards")
 const newDeckBtn = document.getElementById("new-deck")
 const drawCardBtn = document.getElementById("draw-cards")
+const header = document.getElementById("header")
+const remainingText = document.getElementById("remaining")
 
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
-            deckId = data.deck_id
+          
+            
+            if(data.remaining > 0){
+                remainingText.textContent = `Remaining cards: ${data.remaining}`
+                deckId = data.deck_id
+                console.log(deckId)   
+            }else if(data.remaining <= 0){
+               document.getElementById("new-deck").classList.add('disabled-button')
+                document.getElementById("new-deck").disabled = true
+            }
+          
         })
 }
 
 newDeckBtn.addEventListener("click", handleClick)
 
+/**
+ * Challenge:
+ * 
+ * Disable the Draw button when we have no more cards to draw from
+ * in the deck.
+ * 
+ * Disable both the functionality of the button (i.e. change
+ * `disabled` to true on the button) AND the styling (i.e. add
+ * a `disabled` CSS class to make it look unclickable)
+ */
+
 drawCardBtn.addEventListener("click", () => {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
+          
+               remainingText.textContent = `Remaining cards: ${data.remaining}`
+               if(data.remaining > 0){  
             cardsContainer.children[0].innerHTML = `
                 <img src=${data.cards[0].image} class="card" />
             `
             cardsContainer.children[1].innerHTML = `
                 <img src=${data.cards[1].image} class="card" />
             `
-            
-            console.log(data.cards[0].value)
-            console.log(data.cards[1].value)
+            const winnerText = determineCardWinner(data.cards[0], data.cards[1])
+            header.textContent = winnerText 
+              
+            }else if(data.remaining <= 0){
+             
+                document.getElementById("draw-cards").classList.add('disabled-button')
+                document.getElementById("draw-cards").disabled = true
+               
+            }
+         
         })
 })
-/**
- * Challenge:
- * 
- * Try to determine which of the 2 cards is the "winner" (has higher value)
- * Aces are the card with the highest "score"
- * 
- * In parts:
- * 
- * 1. Create a function that takes 2 card objects as parameters, 
- * `card1` and `card2`. These card objects have a property called
- * `value`, which can be any one of the following strings, in
- * order of rising "score":
- * 
- * "2", "3", "4", "5", "6", "7", "8", "9", 
- * "10", "JACK", "QUEEN", "KING", "ACE"
- * 
- * I.e. "2" is the lowest score and "ACE" is the highest.
- * 
- * The function should determine which of the 2 cards (`card1`
- * or `card2`) has the higher score, or if they have the same score.
- * 
- * Log which card wins (or "It's a tie!" 
- * if they're the same) to the console
- */
+
+function determineCardWinner(card1, card2) {
+    const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9", 
+    "10", "JACK", "QUEEN", "KING", "ACE"]
+    const card1ValueIndex = valueOptions.indexOf(card1.value)
+    const card2ValueIndex = valueOptions.indexOf(card2.value)
+    
+    if (card1ValueIndex > card2ValueIndex) {
+        return "Card 1 wins!"
+    } else if (card1ValueIndex < card2ValueIndex) {
+        return "Card 2 wins!"
+    } else {
+        return "War!"
+    }
+}
+
